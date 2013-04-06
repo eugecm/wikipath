4f4a1d438eaff5d3b32b0dcbf46e270b5c3e328d
Eugenio <eugeniocanom@gmail.com>
2013-04-06 14:35:10 +0200

    Added description and name to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject app "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject app "0.1-pre"
+  :description "A program to find a path between to wiki articles"
   :dependencies [[org.clojure/clojure "1.5.0"] 
                  [org.clojure/data.xml "0.0.7"]])

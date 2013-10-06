fc0409621ebaa0c8eed033dc96231cf35cb45fbf
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2013-10-06 22:39:06 +0200

    Renamed local repo

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject wikipath "0.1-pre"
   :description "A program to find a path between to wiki articles"
   :main app.wikipath
-  :dependencies [[ubigraph "0.2.4"]
-                [org.apache.xmlrpc/xmlrpc-client "3.1.3"]
+  :dependencies [[org.apache.xmlrpc/xmlrpc-client "3.1.3"]
                 [org.clojure/data.xml "0.0.7"]
-                [org.clojure/clojure "1.3.0"]]
-  :repositories {"local" "file:maven_rep"})
+                [org.clojure/clojure "1.3.0"]
+                [ubigraph "0.2.4"]]
+  :repositories {"local" "file:repo"})
